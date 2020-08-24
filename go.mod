963cb1ae86ec27a4e5f87ca6bbb1e3189359c772
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-08-24 16:09:43 +0200

    F add signup handler

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/justinas/alice v1.2.0 // indirect
 	github.com/subosito/gotenv v1.2.0
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 )
--- web/handlers.go
+++ web/handlers.go
@@ -1,9 +1,40 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	dto "github.com/guillem-gelabert/go-zeug/web/dtos"
+)
 
 func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("signupUser invoked"))
+	userDTO := &dto.UserDTO{}
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Error reading body:", err)
+	}
+
+	json.Unmarshal(data, userDTO)
+
+	userDTO.ValidateDisplayName(dto.Required(), "display name is required")
+	userDTO.ValidateEmail(dto.Required(), "email is required")
+	userDTO.ValidateEmail(dto.IsEmail(), "email is invalid")
+	userDTO.ValidatePassword(dto.Required(), "password is required")
+	userDTO.ValidatePassword(dto.MaxLength(10), "password is too short")
+
+	err = app.users.Insert(
+		userDTO.DisplayName,
+		userDTO.Email,
+		userDTO.Password,
+	)
+
+	if err != nil {
+		fmt.Println("Error creating user:", err)
+	}
+
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 
 func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
