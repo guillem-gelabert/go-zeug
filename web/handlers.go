ec3153927cc947cd903f3f2ba1ac1df35339dca1
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-08-27 11:06:41 +0200

    F add getSession handler

--- web/handlers.go
+++ web/handlers.go
@@ -5,9 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"os"
 	"runtime/debug"
+	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/guillem-gelabert/go-zeug/pkg/models"
@@ -73,7 +75,7 @@ func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) getNextWords(w http.ResponseWriter, r *http.Request) {
-	u, err := app.users.Get(app.loggedIn)
+	u, err := app.users.Get(app.loggedIn.ID)
 	if err != nil {
 		if errors.Is(err, models.ErrNoRecord) {
 			http.Redirect(w, r, "/login", http.StatusSeeOther)
@@ -88,6 +90,43 @@ func (app *application) getNextWords(w http.ResponseWriter, r *http.Request) {
 	}
 
 	rs, err := json.Marshal(ws)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
+	w.Write(rs)
+}
+
+func (app *application) getSession(w http.ResponseWriter, r *http.Request) {
+	uid := app.loggedIn.ID
+	dueCards, err := app.cards.GetDueBy(uid, time.Now())
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	var newCards []*models.Card
+	words, err := app.words.Next(app.loggedIn.LastSeenPriority, app.loggedIn.NewWordsPerSession)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	for _, word := range words {
+		card, err := app.cards.Create(uid, word)
+		if err != nil {
+			app.serverError(w, err)
+		}
+		newCards = append(newCards, card)
+	}
+
+	var session []*models.Card
+	session = append(append(session, dueCards...), newCards...)
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(session), func(i, j int) { session[i], session[j] = session[j], session[i] })
+
+	rs, err := json.Marshal(session)
 	if err != nil {
 		app.serverError(w, err)
 	}
