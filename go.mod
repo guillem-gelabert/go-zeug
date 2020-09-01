4a15b58dfd079eedbe1740a950aab161e23730ae
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-09-01 12:34:20 +0200

    F add bassinet

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.8.0
+	github.com/guillem-gelabert/bassinet v0.0.0-20200821113350-9494247b7f1c
 	github.com/stretchr/testify v1.6.1 // indirect
 	github.com/subosito/gotenv v1.2.0
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
--- web/routes.go
+++ web/routes.go
@@ -4,10 +4,27 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/guillem-gelabert/bassinet"
 )
 
 func (app *application) routes() http.Handler {
 	r := mux.NewRouter()
+
+	nosniff := bassinet.DontSniffMimetype()
+	xssFilter, err := bassinet.XSSFilter()
+	if err != nil {
+		app.errorLog.Fatal(err)
+	}
+	noReferrer, err := bassinet.ReferrerPolicy([]int{bassinet.PolicyNoReferrer})
+	if err != nil {
+		app.errorLog.Fatal(err)
+	}
+
+	r.Use(
+		nosniff,
+		xssFilter,
+		noReferrer,
+	)
 
 	r.HandleFunc("/signup", app.signupUser)
 	r.HandleFunc("/login", app.loginUser)
