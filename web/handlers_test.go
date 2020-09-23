9a7da4a63b1b48a06fe624fbf8fd2c28efcd6b79
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-09-23 22:57:33 +0200

    a reformat mock session cards

--- web/handlers_test.go
+++ web/handlers_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-	"net/http/httptest"
 	"reflect"
 	"testing"
 
@@ -12,11 +11,16 @@ import (
 
 var mockSessionDTO []dto.CardDTO = []dto.CardDTO{
 	{ID: 0, Article: "das", Substantive: "Haus", WordID: 0, UserID: 1, Stage: "SEEN"},
-	{ID: 1, Article: "das", Substantive: "Jahr", WordID: 1, UserID: 1, Stage: "UNSEEN"}, {ID: 2, Article: "das", Substantive: "Prozent", WordID: 2, UserID: 1, Stage: "SEEN"},
-	{ID: 3, Article: "der", Substantive: "Euro", WordID: 3, UserID: 1, Stage: "UNSEEN"}, {ID: 4, Article: "die", Substantive: "Zeit", WordID: 4, UserID: 1, Stage: "SEEN"},
-	{ID: 5, Article: "die", Substantive: "Kategorie", WordID: 5, UserID: 1, Stage: "UNSEEN"}, {ID: 6, Article: "die", Substantive: "Stadt", WordID: 6, UserID: 1, Stage: "SEEN"},
-	{ID: 7, Article: "das", Substantive: "Ende", WordID: 7, UserID: 1, Stage: "UNSEEN"}, {ID: 8, Article: "die", Substantive: "Frau", WordID: 8, UserID: 1, Stage: "SEEN"},
-	{ID: 9, Article: "das", Substantive: "Leben", WordID: 9, UserID: 1, Stage: "UNSEEN"}, {ID: 10, Article: "das", Substantive: "Leben", WordID: 10, UserID: 1, Stage: "SEEN"},
+	{ID: 1, Article: "das", Substantive: "Jahr", WordID: 1, UserID: 1, Stage: "UNSEEN"},
+	{ID: 2, Article: "das", Substantive: "Prozent", WordID: 2, UserID: 1, Stage: "SEEN"},
+	{ID: 3, Article: "der", Substantive: "Euro", WordID: 3, UserID: 1, Stage: "UNSEEN"},
+	{ID: 4, Article: "die", Substantive: "Zeit", WordID: 4, UserID: 1, Stage: "SEEN"},
+	{ID: 5, Article: "die", Substantive: "Kategorie", WordID: 5, UserID: 1, Stage: "UNSEEN"},
+	{ID: 6, Article: "die", Substantive: "Stadt", WordID: 6, UserID: 1, Stage: "SEEN"},
+	{ID: 7, Article: "das", Substantive: "Ende", WordID: 7, UserID: 1, Stage: "UNSEEN"},
+	{ID: 8, Article: "die", Substantive: "Frau", WordID: 8, UserID: 1, Stage: "SEEN"},
+	{ID: 9, Article: "das", Substantive: "Leben", WordID: 9, UserID: 1, Stage: "UNSEEN"},
+	{ID: 10, Article: "das", Substantive: "Leben", WordID: 10, UserID: 1, Stage: "SEEN"},
 }
 
 func TestGetSession(t *testing.T) {
@@ -78,4 +82,3 @@ func TestGetSession(t *testing.T) {
 		})
 	}
 }
-
